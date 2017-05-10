a0c2404ca590430033514f9403071b47f8106f89
Munir Wanis <fallen.angel.pp@gmail.com>
2017-05-10 11:22:52 -0300

    :construction: Adds Contact entity

---
+++ Infnet.WebApi.Tp1.Business/Entities/Contact.cs
@@ -0,0 +1,15 @@
+﻿namespace Infnet.WebApi.Tp1.Business.Entities
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+    }
+}
