99ee33ff5ba2a19fe9594d6e653d9f5e635786c9
Munir Wanis <fallen.angel.pp@gmail.com>
2017-05-10 11:28:48 -0300

    :construction: Implements context

--- Infnet.WebApi.Tp1.Data/Context.cs
+++ Infnet.WebApi.Tp1.Data/Context.cs
@@ -1,9 +1,10 @@
-﻿using System.Data.Entity;
+﻿using Infnet.WebApi.Tp1.Entities;
+using System.Data.Entity;
 
 namespace Infnet.WebApi.Tp1.Data
 {
     public class Context : DbContext
     {
-        public DbSet<Contact>
+        public DbSet<Contact> Contacts { get; set; }
     }
 }
