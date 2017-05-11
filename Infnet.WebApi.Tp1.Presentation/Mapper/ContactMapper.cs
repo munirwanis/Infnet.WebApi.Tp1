a60cd93d45e5435a4339333a5b94084a62d11065
Munir Wanis <fallen.angel.pp@gmail.com>
2017-05-11 10:54:46 -0300

    :heavy_plus_sign: Adds mapper logic

--- Infnet.WebApi.Tp1.Presentation/Mapper/ContactMapper.cs
+++ Infnet.WebApi.Tp1.Presentation/Mapper/ContactMapper.cs
@@ -1,6 +1,21 @@
-﻿namespace Infnet.WebApi.Tp1.Presentation.Mapper
+﻿using Infnet.WebApi.Tp1.Entities;
+using Infnet.WebApi.Tp1.Presentation.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Infnet.WebApi.Tp1.Presentation.Mapper
 {
     internal class ContactMapper
     {
+        internal List<ContactViewModel> MapContactView(List<Contact> contacts) =>
+            (from cs in contacts
+             select new ContactViewModel
+             {
+                 Email = cs.Email,
+                 FirstName = cs.FirstName,
+                 Id = cs.Id,
+                 LastName = cs.LastName,
+                 PhoneNumber = cs.PhoneNumber
+             }).ToList();
     }
 }
